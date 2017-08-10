2908097d5497c9530be445df82cb74c893d22af4
Nicolaas Thiemen Francken @ B&C <github@sunnysideup.co.nz>
2017-08-10 15:35:27 +1200

    FIRST COMMIT

---
+++ code/model/CallToActionPageExtension.php
@@ -0,0 +1,87 @@
+<?php
+
+/**
+ *@author nicolaas [at] sunnysideup.co.nz
+ *
+ *
+ **/
+
+class CallToActionPageExtension extends DataExtension
+{
+
+    private static $db = [
+        'LargeText' => 'Varchar(255)',
+        'LargeTextFontColour' => 'enum("white,black", "white")',
+        'LargeImageFocusPoint' => 'enum("Centre,N,NW,W,SW,S,SE,E,NE", "Centre")',
+        'CallToAction' => 'Varchar(50)'
+    ];
+
+    private static $has_one = [
+        'LargeImage' => 'Image'
+        'CallToActionLink' => 'SiteTree'
+    ];
+
+    private static $field_labels = [
+        'LargeText' => 'Text'
+        'LargeTextFontColour' => 'Text Colour',
+        'LargeImageFocusPoint' => 'Focus Point',
+        'CallToAction' => 'Call to Action',
+        'LargeImage' => 'Image',
+        'CallToActionLink' => 'Link'
+    ];
+
+    private static $colour_font_options = [
+        'white' => 'white',
+        'black' => 'black'
+    ];
+
+    private static $field_labels_right = [
+        'LargeText' => 'A short sentence'
+        'LargeTextFontColour' => 'Text Colour',
+        'LargeImageFocusPoint' => 'What part of the image should be visible no matter what',
+        'LargeImage' => 'please ensure it is at least 2800px wide',
+        'CallToAction' => 'The text on the button - e.g. Sign Up Now (Optional)',
+        'CallToActionLink' => 'Optional link on button, if left blank users will simply scroll down.'
+    ];
+
+    public function updateCMSFields(FieldList $fields)
+    {
+        $fieldLabels = $this->owner->FieldLabels();
+        $fieldLabelsRight = $this->Config()->get('field_labels_right');
+        $tabTitle = _t('CallToActionPageExtension.CALL_TO_ACTION', 'Call to Action');
+        $fields->addFieldsToTab(
+            'Root.'.$tabTitle,
+            [
+                UploadField::create(
+                    'LargeImage',
+                    $fieldLabels['LargeImage']
+                )->setRightTitle($fieldLabelsRight['LargeImage']),
+                TextareaField::create(
+                    'LargeText',
+                    $fieldLabels['LargeText']
+                )->setRightTitle($fieldLabelsRight['LargeText']),
+                DropdownField::create(
+                    'LargeTextFontColour',
+                    $fieldLabels['LargeTextFontColour']
+                    $this->Config()->get('colour_font_options')
+                )->setRightTitle($fieldLabelsRight['LargeTextFontColour']),
+                TextField::create(
+                    'CallToAction',
+                    $fieldLabels['CallToAction']
+                )->setRightTitle($fieldLabelsRight['CallToAction']),
+                TreeDropdownField::create(
+                    'CallToActionLinkID',
+                    $fieldLabels['CallToActionLinkID']
+                    'SiteTree'
+                )->setRightTitle($fieldLabelsRight['CallToActionLinkID']),
+                DropdownField::create(
+                    'LargeImagFocusPoint',
+                    $fieldLabels['LargeImagFocusPoint']
+                    $this->dbObject('LargeImageFocusPoint')->enumValues()
+                )->setRightTitle($fieldLabelsRight['LargeImagFocusPoint'])
+            )
+        );
+
+        return $fields;
+    }
+}
